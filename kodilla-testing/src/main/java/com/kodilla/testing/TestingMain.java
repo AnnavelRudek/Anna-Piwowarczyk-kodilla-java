20f3a3d696acb9344bd9f099d1b0ba6fb5a60d04
AnnavelRudek <anna.tuszynska1@gmail.com>
2018-03-16 21:36:16 +0100

    First test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+import static javax.print.attribute.standard.MediaSizeName.A;
+
 public class TestingMain {
-    public static void main(String() args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result   = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("error!");
+        }
+
+        System.out.println("\n Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+        int addAtoB;
+        int substractAFromB;
+
+        System.out.println("Add = " + calculator.addAtoB(9, 9));
+        System.out.println("Substract = " + calculator.substractAfromB(156, 3));
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public double substractAfromB(int A, int B){
+        return A - B;
+    }
+    public double addAtoB(int A, int B){
+        return A + B;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
