811ad206a068865ddb9934b29abdd04ce21579d8
AnnavelRudek <anna.tuszynska1@gmail.com>
2018-03-18 00:13:19 +0100

    6.3 full

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 class OddNumbersExterminator {
-    public List<Integer> examinate (ArrayList<Integer> numbers){
-        List<Integer> evenNumbers = new ArrayList<Integer>();
+    public List<Integer> examinate (List<Integer> numbers){
+        List<Integer> evenNumbers = new ArrayList<>();
         for(int n=0; n < numbers.size(); n++) {
             if (numbers.get(n)%2 == 0){
                 evenNumbers.add(numbers.get(n));
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -10,35 +10,43 @@ public class CollectionTestSuite {
     public void before() {
         System.out.println("Example test start: ");
     }
+
     @After
-    public void after(){
-        System.out.println("Tests are finished.");
+    public void after() {
+        System.out.println("Test is finished.");
     }
 
     @Test
-    public void testOddNumbersExterminatorEmptyList(){
+    public void testOddNumbersExterminatorEmptyList() {
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         List<Integer> myList = new ArrayList<>();
         //When
-        List<Integer> resultList = oddNumbersExterminator.examinate((ArrayList<Integer>) myList);
+        List<Integer> resultList = oddNumbersExterminator.examinate(myList);
         //Then
         Assert.assertEquals(0, resultList.size());
+        System.out.println("List is empty. Test ok.");
     }
 
     @Test
-    public void testOddNumbersExterminatorNormalList(){
+    public void testOddNumbersExterminatorNormalList() {
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        List<Integer> myList = new ArrayList<>();
+        myList.add(2);
+        myList.add(4);
+        myList.add(6);
+        myList.add(8);
+        myList.add(10);
 
+        myList.add(1);
+        myList.add(3);
+        myList.add(5);
+        myList.add(7);
         //When
-
+        List<Integer> resultList = oddNumbersExterminator.examinate(myList);
         //Then
-        Assert.assertEquals();
+        Assert.assertEquals(5, resultList.size());
+        System.out.println("Full list - Test, ok. Number of even numbers: " + resultList.size());
     }
 }
-
-//testOddNumbersExterminatorNormalList (sprawdzi czy klasa zachowuje się poprawnie gdy
-// lista zawiera liczby parzyste i nieparzyste)
-// - czy napisac jeden test do tego, czy dwa.
-// - jesli jeden to czy ma byc bez podzialu?
