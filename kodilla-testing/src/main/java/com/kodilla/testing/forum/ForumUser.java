17fe8ed5e6b30cd86bec9714dca0644585904f74
AnnavelRudek <anna.tuszynska1@gmail.com>
2018-03-19 18:56:26 +0100

    6.4pre

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
@@ -1,4 +1,53 @@
 package com.kodilla.testing.forum;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class ForumUser {
+    private String name;
+    private String realName;
+    private ArrayList<ForumPost> posts = new ArrayList<ForumPost>();
+    private LinkedList<ForumComment> comments = new LinkedList<ForumComment>();
+
+    public ForumUser(String name, String realName)
+    {
+      this.name = name; //name visible on forum
+      this.realName = realName; //real name
+    }
+
+    public void addPost(String author, String postBody){ // do nothing
+
+    }
+
+    public void addComment(ForumPost thePost, String author, String commentBody) { //do nothing
+
+    }
+
+    public int getPostsQuantity(){ // return 100 temporatily
+        return 100;
+    }
+
+    public int getCommentQuantity(){ // return 100 temporarily
+        return 100;
+    }
+
+    public ForumPost getPost(int postNumber){ // returning null means that the operation was unsuccessful
+        return null;
+    }
+
+    public ForumComment getComment(int commentNumber){ // ^^
+        return null;
+    }
+
+    public boolean removeComment(ForumComment theComment){ //return true temporarily
+        return true;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -1,41 +1,27 @@
-package com.kodilla.testitng.forum;
-import com.kodilla.testing.user.SimpleUser;
-import org.junit.*;
-public class ForumTestSuite {
-    @Before
-    public void before(){
-        System.out.println("test case: begin");
-    }
-    @After
-    public void after(){
-        System.out.println("test case: end");
-    }
-    @BeforeClass
-    public static void beforeClass(){
-        System.out.println("test suite: begin");
-    }
-    @AfterClass
-    public static void afterClass(){
-        System.out.println("test suite: end");
-    }
-    @Test
-    public void testCaseUserName(){
-        //Given
-        SimpleUser simpleUser = new SimpleUser("theForumUser", "Adam Lipka");
-        //When
-        String result = simpleUser.getUsername();
-        System.out.println("testing" + result);
-        //Then
-        Assert.assertEquals("theForumUser", result);
-    }
-    @Test
-    public void testCaseRealName(){
-        //Given
-        SimpleUser simpleUser = new SimpleUser("theForumUser", "Adam Lipka");
-        //When
-        String result = simpleUser.getRealname();
-        System.out.println("testing" + result);
-        //Then
-        Assert.assertEquals("Adam Lipka", result);
-    }
+package com.kodilla.testing.forum.tdd.;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+// test suite for classes of forum
+
+    public class ForumTestSuite {
+        private static int testCounter = 0;
+
+        @BeforeClass
+        public static void beforeAllTests(){
+            System.out.println("This is the begining of tests.");
+        }
+
+        @AfterClass
+        public static void afterAllTests(){
+            System.out.println("All tests are finished.");
+        }
+
+        @Before
+        public void beforeEveryTest(){
+            testCounter++;
+            System.out.println("Preparing to execute test #" + testCounter);
+        }
 }
