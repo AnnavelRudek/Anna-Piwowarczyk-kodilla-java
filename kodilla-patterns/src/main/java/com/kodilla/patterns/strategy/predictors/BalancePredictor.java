fa80bd338b53cc8104c378701a9248bcc698a9a4
AnnavelRudek <anna.tuszynska1@gmail.com>
2018-05-06 16:32:50 +0200

    11.3

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/predictors/BalancePredictor.java
@@ -0,0 +1,11 @@
+package com.kodilla.patterns.strategy.predictors;
+
+import com.kodilla.patterns.strategy.BuyPredictor;
+
+public class BalancePredictor implements BuyPredictor {
+    @Override
+    public String predictWhatToBuy(){
+        return "[Balanced predictor] Buy shared units of Fund XYZ";
+    }
+
+}
